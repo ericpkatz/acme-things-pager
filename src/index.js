c28ce5f970646f5015727833fc61f2b0f7774f0b
ericpkatz <ericpkatz@gmail.com>
2021-02-15 09:34:51 -0500

    add things to front end

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { HashRouter, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const root = document.querySelector('#root');
 
 const Home = ()=> (<div><h1>Acme Home</h1></div>);
-const Things = ()=> (<div><h1>Things</h1></div>);
+
+class Things extends Component{
+  constructor(){
+    super();
+    this.state = {
+      things: []
+    };
+  }
+  async componentDidMount(){
+    const things = (await axios.get('/api/things')).data;
+    this.setState({ things });
+  }
+  render(){
+    const { things } = this.state;
+    return (
+      <ul>
+        {
+          things.map( thing => {
+            return (
+              <li key={ thing.id } style={{ backgroundColor: thing.color }}>
+                { thing.name }
+                <br />
+                { thing.price }
+              </li>
+            );
+          })
+        }
+      </ul>
+    );
+  }
+}
 
 
 class App extends Component{
