94ea70d7be92baa1c2a809df0fe79f7a1fa579a4
ericpkatz <ericpkatz@gmail.com>
2021-02-15 21:19:30 -0500

    commit

--- src/store.js
+++ src/store.js
@@ -3,8 +3,9 @@ import thunk from 'redux-thunk';
 import axios from 'axios';
 
 const thingsReducer = (state={ count: 0, data: {}}, action)=> {
+  console.log(action);
   if(action.type === 'FETCH_THINGS'){
-    state = {...state, data: {...stage.data, [action.idx]: action.things}};
+    state = {...state, data: {...state.data, [action.idx]: action.things}};
   }
   if(action.type === 'SET_COUNT'){
     state = {...state, count: action.count};
@@ -20,7 +21,7 @@ const _fetchThings = ({idx, things})=> {
   return {
     idx,
     things,
-    type: ''
+    type: 'FETCH_THINGS'
   };
 };
 
@@ -36,7 +37,7 @@ const fetchThings = (idx)=> {
   return async(dispatch, getState)=> {
     const things = getState().things.data[idx];
     if(things){
-      dispatch(_fetchThings(things))
+      dispatch(_fetchThings({idx, things}))
     }
     else {
       const { things, count } = (await axios.get(`/api/things?idx=${idx}`)).data
